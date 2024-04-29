725040cd5a4dcdcb0f7edbfc51f484162a3ec721
Meson Jiang <jiang131072@gmail.com>
2024-04-29 22:57:18 +0800

    bump to 0.6.0

--- src/szu_drcom_login/__init__.py
+++ src/szu_drcom_login/__init__.py
@@ -5,17 +5,21 @@ import schedule
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
+titles_ok = ("登录成功页", "Login success page", "注销页", "Logout page")
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless=new")
+options.add_argument("--disable-gpu")
+
 
 def test(title: str):
-    if title in ("注销页", "Logout page"):
+    if title in titles_ok:
         return True
     else:
         return False
 
 
 def login(username, password):
-    options = webdriver.ChromeOptions()
-    options.add_argument("--headless=new")
     driver = webdriver.Chrome(options)
     driver.get(url)
 
@@ -35,12 +39,13 @@ def login(username, password):
 
         in_usn.send_keys(username)
         in_pwd.send_keys(password)
-        # box_lgoin.
         btm_login.click()
+
         if test(driver.title):
             print("登录成功")
         else:
             print("登录失败，稍后再试")
+
     driver.close()
 
 
@@ -48,7 +53,7 @@ def main():
     parser = argparse.ArgumentParser()
 
     parser.add_argument(
-        "location", type=str, help="登录位置（宿舍或教学区），对应dorm/ta"
+        "location", type=str, help="登录位置（宿舍或教学区），对应 dorm/ta"
     )
     parser.add_argument("username", type=str, help="账号")
     parser.add_argument("password", type=str, help="密码")
@@ -61,14 +66,14 @@ def main():
         case "ta":
             url = "http://drcom.szu.edu.cn"
         case _:
-            raise ValueError("无效地址。宿舍区：dorm，教学区：ta")
+            raise ValueError("无效地址")
 
     username = args.username
     password = args.password
 
     login(username, password)
-    schedule.every(1).minutes.do(login, username, password)
 
+    schedule.every(1).minutes.do(login, username, password)
     while True:
         try:
             schedule.run_pending()
